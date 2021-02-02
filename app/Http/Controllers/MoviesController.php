674d32fa14786aee6794533d1968e123eead680c
Miroslav Pantos <bboypamik@gmail.com>
2021-02-02 18:08:09 +0100

    first

---
+++ app/Http/Controllers/MoviesController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MoviesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $movies = Movie::all();
+        return response()->json($movies);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all;
+        $newMovie = Movie::create($data);
+        return response()->json($newMovie);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $movie = Movie::findOrFail($id);
+        return response()->json($movie);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $movie = Movie::findOrFail($id);
+        $movie->update($data);
+        return response()->json($movie);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $movie = Movie::findOrFail($id);
+        $movie->delete();
+        return response()->json($movie);
+    }
+}
